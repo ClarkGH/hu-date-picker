5111e71a2d962d1adbb762eca38b3033c726aea9
clarkgh <clarkhinchcliff@gmail.com>
2023-04-03 10:18:23 -0400

    calendar

--- src/date-picker.ts
+++ src/date-picker.ts
@@ -5,7 +5,13 @@ import { createPopper, Instance } from '@popperjs/core';
 @customElement('date-picker')
 export class MyElement extends LitElement {
   @property({ type: String })
-  private selectedDate = '';
+  selectedDate = '';
+
+  @property({ type: String })
+  private selectedYear = new Date().getFullYear();
+
+  @property({ type: String })
+  private selectedMonth = new Date().getMonth() + 1;
 
   @property({ type: String })
   private divId = 'my-div';
@@ -13,16 +19,18 @@ export class MyElement extends LitElement {
   @property({ type: String })
   private buttonId = 'my-button';
 
-  @property({ type: String })
+  @property({ type: Boolean })
   isPopperVisible = false;
+  
+  @property({ type: Boolean })
+  isFieldValid = false;
 
   private _showPopper() {
     const input = this.shadowRoot?.querySelector(`#${this.divId}`) as HTMLInputElement;
     const button = this.shadowRoot?.querySelector(`#${this.buttonId}`) as HTMLButtonElement;
 
-    this.isPopperVisible = true;
+    this._togglePopper();
 
-    // Create a new Popper instance
     const popper: Instance = createPopper(input, button.nextElementSibling as HTMLElement, {
       placement: 'bottom',
       modifiers: [
@@ -35,90 +43,110 @@ export class MyElement extends LitElement {
       ],
     });
 
-    // Show the popper
     popper.update();
   }
 
-  private _hidePopper() {
-    this.isPopperVisible = false;
+  private _togglePopper() {
+    this.isPopperVisible = !this.isPopperVisible;
   }
 
-  private _onDateSelected(e: Event) {
-    const dateInput = e.target as HTMLInputElement;
-    const selectedDate = dateInput.value;
-    this.selectedDate = selectedDate;
-    this._hidePopper();
+  private _onInputBlur(e: FocusEvent) {
+    const input = e.target as HTMLInputElement;
+    const value = input.value.trim();
+    const pattern = /^\d{2}\/\d{2}\/\d{4}$/;
+  
+    if (!pattern.test(value)) {
+      console.error('Invalid date format. Please enter a date in the format MM/DD/YYYY');
+      input.value = '';
+    } else {
+      this.selectedDate = value;
+      this.isFieldValid = true;
+    }
   }
 
   private _renderDatePicker() {
-    const today = new Date();
-    let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth() + 1;
-    let daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
-    let daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+    const daysInMonth = new Date(this.selectedYear, this.selectedMonth, 0).getDate();
+    const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
   
-    const updateCalendar = (year: number, month: number) => {
-      currentYear = year;
-      currentMonth = month;
-      daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
-      daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
-      this.requestUpdate();
-    };
+    const monthNames = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
   
-    const onMonthChange = (delta: number) => {
-      const newMonth = currentMonth + delta;
-      if (newMonth < 1) {
-        updateCalendar(currentYear - 1, 12);
-      } else if (newMonth > 12) {
-        updateCalendar(currentYear + 1, 1);
+    const monthOptions = monthNames.map((month, index) => {
+      return html`
+        <option value="${index + 1}">${month}</option>
+      `;
+    });
+  
+    const currentYear = new Date().getFullYear();
+    const yearOptions = [];
+    const startYear = currentYear - 500;
+    const endYear = currentYear + 500;
+  
+    for (let i = startYear; i <= endYear; i++) {
+      if (i === currentYear) {
+        yearOptions.push(html`
+          <option value="${i}" selected>${i}</option>
+        `);
       } else {
-        updateCalendar(currentYear, newMonth);
+        yearOptions.push(html`
+          <option value="${i}">${i}</option>
+        `);
       }
-    };
+    }
   
-    const onYearChange = (delta: number) => {
-      const newYear = currentYear + delta;
-      updateCalendar(newYear, currentMonth);
-    };
+    const weeksInMonth = Math.ceil(daysInMonth / 7);
+    const daysGrid = [];
   
-    const onDayClick = (day: number) => {
-      const newDate = new Date(currentYear, currentMonth - 1, day);
-      const formattedDate = newDate.toLocaleDateString('en-US', {
-        month: '2-digit',
-        day: '2-digit',
-        year: 'numeric',
-      });
-      const input = this.shadowRoot?.querySelector(`#${this.divId} input`) as HTMLInputElement;
-      input.value = formattedDate;
-      input.dispatchEvent(new Event('input', { bubbles: true, composed: true }));
-      this._hidePopper();
-    };
+    // loop through the days of the month and create a div element for each day
+    for (let day of daysArray) {
+      const date = new Date(this.selectedYear, this.selectedMonth - 1, day);
+      const dayOfWeek = date.getDay();
+  
+      // add empty div elements to fill in the days before the first day of the month
+      if (day === 1) {
+        for (let i = 0; i < dayOfWeek; i++) {
+          daysGrid.push(html`<div></div>`);
+        }
+      }
+  
+      daysGrid.push(html`<div class="day">${day}</div>`);
+  
+      // add empty div elements to fill in the days after the last day of the month
+      if (day === daysInMonth) {
+        for (let i = dayOfWeek + 1; i <= 6; i++) {
+          daysGrid.push(html`<div></div>`);
+        }
+      }
+    }
   
     return html`
       <div class="date-picker">
-        <div class="month-year">
-          <button class="prev-year" @click="${() => onYearChange(-1)}">&lt;&lt;</button>
-          <button class="prev-month" @click="${() => onMonthChange(-1)}">&lt;</button>
-          <div class="month">${currentMonth}</div>
-          <div class="year">${currentYear}</div>
-          <button class="next-month" @click="${() => onMonthChange(1)}">&gt;</button>
-          <button class="next-year" @click="${() => onYearChange(1)}">&gt;&gt;</button>
+        <div class="month-year__picker">
+          <select>${monthOptions}</select>
+          <select>${yearOptions}</select>
         </div>
-        <div class="days">
-          ${daysArray.map(
-            (day) => html`<button class="day" @click="${() => onDayClick(day)}">${day}</button>`
-          )}
+        <div class="day__picker">
+        <div class="week">
+          <div class="day">Sun</div>
+          <div class="day">Mon</div>
+          <div class="day">Tue</div>
+          <div class="day">Wed</div>
+          <div class="day">Thu</div>
+          <div class="day">Fri</div>
+          <div class="day">Sat</div>
+          ${daysGrid}
         </div>
       </div>
+      </div>
     `;
-  }
+  }  
   
-  
-
   render() {
     return html`
       <div class="input-container" id="${this.divId}">
-        <input type="text" placeholder="MM/DD/YYYY" />
+        <input type="text" placeholder="MM/DD/YYYY" .value="${this.selectedDate}" @blur="${this._onInputBlur}" />
         <button id="${this.buttonId}" @click="${this._showPopper}">
           <svg>
             <path d="M19,3H17V1h-2v2H9V1H7v2H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V5C21,3.9,20.1,3,19,3z M5,8h14v2H5V8z M5,12h14v2H5V12z M5,16h9v2H5V16z"/>
@@ -126,12 +154,12 @@ export class MyElement extends LitElement {
         </button>
       </div>
       ${this.isPopperVisible
-        ? html`<div class="popper">
+        ? html`<div>
           ${this._renderDatePicker()}
         </div>`
         : ''}
     `;
-  }
+  }  
 
   static styles = css`
     :host {
@@ -149,6 +177,7 @@ export class MyElement extends LitElement {
     input {
       flex: 1;
       padding-right: 32px;
+      width: 280px;
     }
 
     button {
@@ -171,11 +200,40 @@ export class MyElement extends LitElement {
       pointer-events: none;
     }
 
-    /* Style the popper */
     .popper {
       background-color: white;
       border: 1px solid black;
       padding: 8px;
+    }
+
+    .date-picker {
+      display: flex;
+      flex-direction: column;
+      font-size: 16px;
+      width: 400px;
+      padding: 8px;
+      border: 1px solid black;
+    }
+
+    .month-year__picker {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .day__picker {
+      display: grid;
+      grid-template-columns: repeat(7, 1fr);
+    }
+    
+    .week {
+      display: grid;
+      grid-template-columns: repeat(7, 1fr);
+      grid-auto-rows: minmax(100px, auto);
+    }
+    
+    .day {
+      border: 1px solid black;
+      padding: 10px;
     }
   `;
 }
